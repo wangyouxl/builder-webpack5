d4f9c4c7680d6904d7b2a704924d14a0443b4fd5
wangyouxl <2505567708@qq.com>
2025-02-12 10:28:10 +0800

    chore: update webpack-base-test it entry

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,19 +3,17 @@ const assert = require("assert"); //用assert进行断言
 
 describe("webpack.base.js test case", () => {
   const baseConfig = require("../../lib/webpack.base");
-  console.log(baseConfig);
-
   it("entry", () => {
     //it 表示一个测试用例
     assert.equal(
       baseConfig.entry.search.indexOf(
-        "bulider-webpack5/test/smoke/emplate/src/search/index.js"
+        "builder-webpack5/test/smoke/emplate/src/search/index.js"
       ) > -1,
       true
     );
     assert.equal(
       baseConfig.entry.app.indexOf(
-        "bulider-webpack5/test/smoke/emplate/src/app/index.js"
+        "builder-webpack5/test/smoke/emplate/src/app/index.js"
       ) > -1,
       true
     );
