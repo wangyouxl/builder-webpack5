369ab435ef44797be83c427b4c110c2b0e1959d5
wangyouxl <2505567708@qq.com>
2025-02-12 10:35:57 +0800

    chore: update webpack-base-test it entry

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,6 +3,10 @@ const assert = require("assert"); //用assert进行断言
 
 describe("webpack.base.js test case", () => {
   const baseConfig = require("../../lib/webpack.base");
+
+  console.log(baseConfig.entry.search);
+  console.log(baseConfig.entry.app);
+
   it("entry", () => {
     //it 表示一个测试用例
     assert.equal(
